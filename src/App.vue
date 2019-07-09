0c249412012ef1fb680887f5e824c3a944a71271
hhuyk <38423152@qq.com>
2019-07-09 16:27:59 +0800

    登录界面

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <transition name="component-fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style>
+  @import './assets/css/base.css';
+  @import './assets/css/font-awesome-4.7.0.min.css';
+  .component-fade-enter-active, .component-fade-leave-active {
+    transition: opacity .3s ease;
+  }
+  .component-fade-enter, .component-fade-leave-active {
+    opacity: 0;
+  }
+</style>
